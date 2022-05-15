97a960a95e1d1d5f360102f47409c4537ba2aa0a
kfisch2 <kmlindsey24@gmail.com>
2022-05-14 20:01:44 -0700

    username prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!")
+
+
+// creates a function named "fight"
+
+function fight() {
+    var playerName = window.prompt("What's your robot's name?");
+    window.alert("Welcome, " + playerName);
+    console.log(playerName);
+}
+
+
+
+// calling the function
+fight();
