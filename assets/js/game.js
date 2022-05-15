a052f9cd75c1c38849237d4b6648f29f90111f67
kfisch2 <kmlindsey24@gmail.com>
2022-05-14 20:32:52 -0700

    add attack and health functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,36 @@
+// set player/enemy name, health, and attack variables
+var playerName = window.prompt("What's your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
+console.log(playerName, playerAttack, playerHealth);
 
-// creates a function named "fight"
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    var playerName = window.prompt("What's your robot's name?");
-    window.alert("Welcome, " + playerName);
-    console.log(playerName);
-}
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!")
 
+    // subtract playerAttack from enemyHealth
+    enemyHealth -= playerAttack;
 
+    // log result
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // subtract enemyAttack from playerHealth
+    playerHealth -= enemyAttack;
+
+    // log result
+    console.log(playerHealth);
+
+    playerHealth -= enemyAttack;
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + "now has " + playerHealth + " health remianing."
+    );
+};
 
 // calling the function
 fight();
